31cc549e8dea26f4046f635cc0d0f34ca19cd702
Rahul Pachar <rahulpachar0786@gmail.com>
2023-08-14 14:55:36 +0530

    full app

---
+++ src/Components.js/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import "./Navbar.css"
+import {FaCoins} from 'react-icons/fa'
+
+const Navbar = () => {
+  return (
+    <Link to='/'>
+        <div className='navbar'>
+            <FaCoins className='icon'/>
+            <h1> Coin <span className='purple'>Search</span></h1>
+        </div>
+    </Link>
+  )
+}
+
+export default Navbar
